fb7318aa32b2d317aaa01126a360064fc2b2e54b
hollocaine <hollocaine@github.com>
2019-06-05 10:35:43 +0100

    Updates

---
+++ precache-manifest.b560837dd2d5c5f9eba8bb295dbc6b9b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "65cc334749f14211592294c05428bc2c",
+    "url": "/contactlist/index.html"
+  },
+  {
+    "revision": "d114da63ba37a2a933a3",
+    "url": "/contactlist/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "d67af33f38cdc3d41e71",
+    "url": "/contactlist/static/css/main.36497136.chunk.css"
+  },
+  {
+    "revision": "d114da63ba37a2a933a3",
+    "url": "/contactlist/static/js/2.b960f609.chunk.js"
+  },
+  {
+    "revision": "d67af33f38cdc3d41e71",
+    "url": "/contactlist/static/js/main.bc7b8427.chunk.js"
+  },
+  {
+    "revision": "dc79425b88bac54299ed",
+    "url": "/contactlist/static/js/runtime~main.ea8a2278.js"
+  }
+]);
